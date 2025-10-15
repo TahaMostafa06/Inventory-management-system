f9453a5369d07554139e7af197627cc0d827d8dd
Youssef Samy <office.youssefkassem@gmail.com>
2025-10-15 20:45:57 +0300

    Implement CustomerProduct.

--- src/system/CustomerProduct.java
+++ src/system/CustomerProduct.java
@@ -1,15 +1,46 @@
 package system;
 import java.time.*;
 public class CustomerProduct{
-	private String customerSSN, productID;
-	private LocalDate purchaseDate;
-	private Boolean paid;
-//	public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate)
-//	public String getCustomerSSN()
-//	public String getProductID()
-//	public LocalDate getPurchaseDate()
-//	public String lineRepresentation()
-//	public boolean isPaid()
-//	public void setPaid(boolean paid)
-//	public String getSearchKey()
+	private final String customerSSN;
+	private final String productID;
+	private final LocalDate purchaseDate;
+	private boolean paid;
+
+	public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+		this.customerSSN = customerSSN;
+		this.productID = productID;
+		this.purchaseDate = purchaseDate;
+		this.paid = false;
+	}
+
+	public String getCustomerSSN() {
+		return this.customerSSN;
+	}
+
+	public String getProductID() {
+		return this.productID;
+	}
+
+	public LocalDate getPurchaseDate() {
+		return this.purchaseDate;
+	}
+
+	public String lineRepresentation() {
+		// TODO: check date format
+		return this.customerSSN + ',' + this.productID + ',' + this.purchaseDate;
+	}
+
+	public boolean isPaid() {
+		return this.paid;
+	}
+
+	public void setPaid(boolean paid) {
+		// TODO: check if it must be changeable only once
+		this.paid = paid;
+	}
+
+	public String getSearchKey() {
+		// TODO: ask TA for specs on this
+		return this.productID;
+	}
 }
