5ae272cdfe61a42c35a7bba56bd66ef8c734c03e
Youssef Samy <office.youssefkassem@gmail.com>
2025-10-17 20:21:30 +0300

    Abstracted Database

--- src/common/Database.java
+++ src/common/Database.java
@@ -4,29 +4,62 @@ import java.util.ArrayList;
 
 public abstract class Database<RecordType extends Record> {
 
-    // Fields - remember to make them private when subclassing
+    // A] Fields - remember to turn protected -> private when subclassing
     protected ArrayList<RecordType> records;
     protected final String filename;
 
-    // Constructor blueprint
+    // B] Constructor blueprint
     public Database(String filename) {
         this.filename = filename;
         this.readFromFile();
     }
+    
+    // C] Methods
 
-    abstract public void readFromFile(); // TODO: make concrete
-
-    abstract public void saveToFile(); // TODO: make concrete
-
-    abstract public RecordType createRecordFrom(String line);
-
-    abstract public boolean contains(String key);
+    // Getters
+    public ArrayList<RecordType> returnAllRecords() {
+        // discuss whether returning a deep clone is better
+        return this.records;
+    }
 
     public RecordType getRecord(String key) {
         for (var r : this.records) {
-            if (r.getSearchKey().equals(key))
+            if (r.getSearchKey().equals(key)) {
                 return r;
+            }
         }
         return null;
     }
+
+    // Setters
+    public void insertRecord(RecordType record) {
+        this.records.add(record);
+    }
+
+    public void deleteRecord(String key) {
+        var query = getRecord(key);
+        if (query != null) {
+            this.records.remove(query);
+        }
+    }
+
+    // Concrete methods
+    public boolean contains(String key) {
+        for (var r : this.records) {
+            if (r.getSearchKey().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // Abstract methods
+    abstract public void readFromFile();
+
+    abstract public void saveToFile();
+
+    abstract public RecordType createRecordFrom(String line);
+
+
 }
