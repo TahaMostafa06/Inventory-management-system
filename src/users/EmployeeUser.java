24a6b686fc0d24685c53987b228f2dd7c1a12ca8
Youssef Samy <office.youssefkassem@gmail.com>
2025-10-18 23:13:24 +0300

    EmployeeUser & EmployeeUserDatabase

--- src/users/EmployeeUser.java
+++ src/users/EmployeeUser.java
@@ -1,23 +1,30 @@
 package users;
-public class EmployeeUser{
+
+import common.Record;
+
+public class EmployeeUser implements Record {
 	private String employeeID, name, email, address, phoneNumber;
-	public EmployeeUser(String employeeID, String name, String email, String address, String phoneNumber){
+
+	public EmployeeUser(String employeeID, String name, String email, String address,
+			String phoneNumber) {
 		this.employeeID = employeeID;
 		this.name = name;
 		this.email = email;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
 	}
-	public String lineRepresentation(){
+
+	public String lineRepresentation() {
 		String representation = "";
 		representation += employeeID + ",";
 		representation += name + ",";
 		representation += email + ",";
 		representation += address + ",";
 		representation += phoneNumber;
-                return representation;
+		return representation;
 	}
-	public String getSearchKey(){
+
+	public String getSearchKey() {
 		return this.employeeID;
 	}
 }
