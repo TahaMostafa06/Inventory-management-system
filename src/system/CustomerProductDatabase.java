e6eb511ed744d8eb11c58b6b8657b55f5e055000
Nada Khaled <nadakhaled0357@gmail.com>
2025-10-19 11:40:10 +0300

    CustomerProductDatabase
    Refactor CustomerProductDatabase class and update package name.

--- src/system/CustomerProductDatabase.java
+++ src/system/CustomerProductDatabase.java
@@ -1,15 +1,45 @@
-package system;
-import java.util.*;
-public class CustomerProductDatabase{
-	private ArrayList<CustomerProduct> records;
-	private String filename;
-//	public CustomerProductDatabase (String filename)
-//	public void readFromFile()
-//	public CustomerProduct createRecordFrom(String line)
-//	public ArrayList<CustomerProduct> returnAllRecords()
-//	public boolean contains(String key)
-//	public CustomerProduct getRecord(String key)
-//	public void insertRecord(CustomerProduct record)
-//	public void deleteRecord(String key)
-//	public void saveToFile()
+package InventoryManagementSystem;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class CustomerProductDatabase {
+
+    
+    private static class CustomerProduct {
+
+        public CustomerProduct(String cID, String pID, SimpleDateFormat dmy, boolean pay) {
+        }
+    }
+    
+    
+    private ArrayList<CustomerProduct> records;
+    private String filename;
+
+    public CustomerProductDatabase(String filename) {
+        this.records = records;
+        this.filename = filename;
+    }
+
+    public void readFromFile() throws FileNotFoundException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader("filename"));
+        String line;
+        while((line=br.readLine())!=null){
+            records.add(createRecordFrom(line));
+        }  
+        br.close();
+    }
+    public CustomerProduct createRecordFrom(String line){
+        String[] tokens = line.split(",");
+        String cID = tokens[0];
+        String pID = tokens[1];
+        LocalDate dmy = LocalDate.parse(tokens[2]);
+        boolean pay = Boolean.parseBoolean(tokens[3]);
+    CustomerProduct p = new CustomerProduct(cID,pID,dmy,pay);    
+    return p;
+    }
+    
 }
