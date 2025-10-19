0929a9d2829a33595168391a2a5416417052214d
Youssef Samy <office.youssefkassem@gmail.com>
2025-10-19 20:05:46 +0300

    CustomerProductDatabase

--- src/system/CustomerProductDatabase.java
+++ src/system/CustomerProductDatabase.java
@@ -1,11 +1,5 @@
 package system;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -26,7 +20,8 @@ public class CustomerProductDatabase extends Database<CustomerProduct> {
         String pID = tokens[1];
         LocalDate dmy = LocalDate.parse(tokens[2]);
         boolean pay = Boolean.parseBoolean(tokens[3]);
-        CustomerProduct p = new CustomerProduct(cID, pID, dmy, pay);
+        CustomerProduct p = new CustomerProduct(cID, pID, dmy);
+        p.setPaid(pay);
         return p;
     }
 
